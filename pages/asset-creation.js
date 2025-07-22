ce777c2e2bb58492df091781b7c038031e71018c
TomSumner28 <tom@therewardcollection.com>
2025-07-22 09:41:10 +0100

    Redesign UI with TRC branding

---
+++ pages/asset-creation.js
@@ -0,0 +1,8 @@
+export default function AssetCreation() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1 style={{ color: '#5ec2f7' }}>Asset Creation</h1>
+      <p>Coming soon.</p>
+    </div>
+  );
+}
